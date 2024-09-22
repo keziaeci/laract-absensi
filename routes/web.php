207009bb4c36ae15d7a35bd494c2def696cc65ff
keziaeci <keziaeci2806@gmail.com>
2024-09-22 22:13:48 +0700

    setup shadcn (undone)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->middleware('auth');
+
+Route::middleware('guest')->group(function () {
+
+    Route::controller(AuthController::class)->group(function (){
+        Route::get('/login', 'login')->name('login');
+    });
+    
 });
